0fad4d0efa6bf42e198c1930c12dbb08c0cdc5e9
r6lm <rdgolaramolina@gmail.com>
2022-06-09 16:26:11 +0100

    Add IU results

--- bash_cmd_results.sh
+++ bash_cmd_results.sh
@@ -1,4 +1,11 @@
+# Incremental Update
+printf "#######################################################################\nIU results:\n#"
+cat MovieLens/IU/ckpts/IU_train11-23_test24-30_1epoch_0.001/period30/test_metrics.txt
+printf "\n"
+
 # SMLmf
-printf "SMLmf results:\n########################################################################"
+printf "#######################################################################\nSMLmf results:\n#"
 cat MovieLens/SMLmf/SML/ckpts/SMLmf_train11-23_test24-30_1epoch_1epoch_0.01_0.01/period30/test_metrics.txt
-printf "########################################################################\n"
+printf "\n"
+
+
