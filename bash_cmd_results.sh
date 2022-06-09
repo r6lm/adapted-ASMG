482d581e2d1167565161f4cce33ebdc30332d3cf
r6lm <rdgolaramolina@gmail.com>
2022-06-09 13:20:18 +0100

    add bash for SMLmf t2

---
+++ bash_cmd_results.sh
@@ -0,0 +1,4 @@
+# SMLmf
+printf "SMLmf results:\n########################################################################"
+cat MovieLens/SMLmf/SML/ckpts/SMLmf_train11-23_test24-30_1epoch_1epoch_0.01_0.01/period30/test_metrics.txt
+printf "########################################################################\n"
